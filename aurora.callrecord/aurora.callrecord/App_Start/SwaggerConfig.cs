6b26486a497b79789394e1364ad891dd884e9cc3
Andras Soskuthy <Andras.Soskuthy@interact-intranet.com>
2019-10-08 14:00:18 +0100

    Configured Swagger to include xml comments in it's metadata.

--- aurora.callrecord/aurora.callrecord/App_Start/SwaggerConfig.cs
+++ aurora.callrecord/aurora.callrecord/App_Start/SwaggerConfig.cs
@@ -101,7 +101,7 @@ namespace aurora.callrecord
                         // those comments into the generated docs and UI. You can enable this by providing the path to one or
                         // more Xml comment files.
                         //
-                        //c.IncludeXmlComments(GetXmlCommentsPath());
+                        c.IncludeXmlComments($@"{System.AppDomain.CurrentDomain.BaseDirectory}\bin\aurora.callrecord.xml");
 
                         // Swashbuckle makes a best attempt at generating Swagger compliant JSON schemas for the various types
                         // exposed in your API. However, there may be occasions when more control of the output is needed.
